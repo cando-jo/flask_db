61dfce0a5639e87838554746e091414257cba716
Qusai GAZAWY <candotube@gmail.com>
2023-12-25 18:22:55 +0300

    CRUD Operations

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password123@localhost/users'
+# mysql://username:password@host:port/database_name
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class User(db.Model): #Muscle Memory
     id=db.Column(db.Integer, primary_key=True)
     title=db.Column(db.String(100), nullable=False)
     description=db.Column(db.String(400), nullable=False)
@@ -37,7 +39,42 @@ def add_user():
         
         return redirect(url_for("index"))
     return render_template("user/add.html")
+
+@app.route("/delete/<int:id>", methods=["POST"])
+def delete(id):
+    if request.method == "POST":
+        user = User.query.get(id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return redirect(url_for("index"))
+        else:
+            return "User not found", 404
+
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:id>", methods=["POST"])
+def update(id):
+    if request.method=="POST":
+        user=User.query.get(id)
+        if user:
+            new_title = request.form["new_title"]
+            new_description = request.form["new_description"]
+            if new_title:
+                user.title = new_title
+            if new_description:
+                user.description = new_description
+                
+            db.session.commit()
+            return redirect(url_for('index'))
+        
+        else:
+            return "User not found", 404
+        
+    return redirect(url_for("index"))
+        
+    
     
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
     
